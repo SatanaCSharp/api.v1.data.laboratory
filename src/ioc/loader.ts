ca40df9efd3e9c711013228bead28a84e80f75a0
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-12-20 16:06:38 +0200

    added ioc

---
+++ src/ioc/loader.ts
@@ -0,0 +1,7 @@
+import '../controllers/advert.types.controller';
+import '../controllers/cities.controller';
+import '../controllers/fids.controller';
+import '../controllers/price.types.controller';
+import '../controllers/realty.types.controller';
+import '../controllers/states.controller';
+import '../controllers/wall.types.controller';
