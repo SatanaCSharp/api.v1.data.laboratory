aae2c0c13552c054ce0d37c0277388fddfca068a
Yurii Horchuk <fkca.av15gyua@gmail.com>
2019-12-30 10:40:51 +0200

    Task-#4 done

--- src/repositories/education.queue.fs.repository.ts
+++ src/repositories/education.queue.fs.repository.ts
@@ -1,20 +1,17 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
 import { injectable } from 'inversify';
-import path from 'path';
 import {config} from '../config';
 import { IEducationQueue } from './../schemas/interfaces/ieducation.queue';
 import { IEducationQueueRepository } from './interfaces/ieducation.queue.repository';
 @injectable()
 export class EducationQueueFsRepository implements IEducationQueueRepository {
-    // private path = config.FS_DATA_EDUCATION_QUEUE_PATH;
     private path: string ;
     constructor() {
-        this.path = './dani-pro-chergi-ditei-u-doshkilni-navchalni-zakladi-stanom-na-01-12-2019.json';
+        this.path =  config.FS_DATA_EDUCATION_QUEUE_PATH;
     }
     public findAll = async (): Promise<IEducationQueue[]> => {
-        const educationQueuesFs = fs.readFileSync(this.path, 'utf8');
-        console.log(educationQueuesFs);
-        const educationQueues: IEducationQueue[] = [];
+        const educationQueuesFs = readFileSync(this.path, 'utf8');
+        const educationQueues: IEducationQueue[] = JSON.parse(educationQueuesFs);
         return educationQueues;
     }
 }
